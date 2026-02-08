46fee729da25dd15edb74cb08d6dea8c10b8a1b8
haidao9610 <324009610@qq.com>
2026-02-08 22:48:09 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -60,7 +60,9 @@ lazy_static::lazy_static! {
         option_env!("RENDEZVOUS_SERVER").unwrap_or("rs-ny.rustdesk.com").into()
     );
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("RustDesk".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new(
+        option_env!("APP_NAME").unwrap_or("RustDesk").into()
+    );
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
